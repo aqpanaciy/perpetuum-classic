d37cf5d9f24518eca820dbfe08885f59ad8ae457
AqpAnaciy <iam@aqpanaciy.ru>
2024-09-30 21:17:39 +0300

    Update Dockerfile

--- docker/perpetuum-database/entrypoint.sh
+++ docker/perpetuum-database/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Start the script to create the DB and user
-/usr/config/configure-db.sh &
+    /var/opt/mssql/config/configure-db.sh &
 
 # Start SQL Server
-/opt/mssql/bin/sqlservr
-
-
+    /opt/mssql/bin/sqlservr
--- docker/perpetuum-database/setup.sql
+++ docker/perpetuum-database/setup.sql
@@ -1,8 +1,9 @@
-/*
+USE [master]
+DROP DATABASE IF EXISTS [perpetuumsa]
+GO
 
-Enter custom T-SQL here that would run after SQL Server has started up. 
-
-*/
-
-CREATE DATABASE HelloWorld;
+RESTORE DATABASE [perpetuumsa] FROM  DISK = N'/var/opt/mssql/data/perpetuumsa.bak' WITH  FILE = 1,  MOVE N'perpetuumsa' TO N'/var/opt/mssql/data/psa.mdf',  MOVE N'perpetuumsa_log' TO N'/var/opt/mssql/data/psa.ldf',  NOUNLOAD,  REPLACE, STATS = 4,  BUFFERCOUNT = 64, MAXTRANSFERSIZE = 4194304
+ALTER DATABASE [perpetuumsa] SET MULTI_USER
+GO
+ALTER DATABASE [perpetuumsa] SET COMPATIBILITY_LEVEL = 130
 GO
